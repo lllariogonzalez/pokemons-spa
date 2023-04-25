4154292c2fba6b4ed34e49a1c16bc8c899d30b55
lllariogonzalez <lllariogonzalez@gmail.com>
2023-04-25 16:47:52 -0300

    Add info status server Railway.app

--- client/src/containers/SectionPokemons.jsx
+++ client/src/containers/SectionPokemons.jsx
@@ -10,43 +10,43 @@ import style from "./SectionPokemons.module.css";
 import loadingPikachu from "../images/loadingPikachu.gif";
 import errorPikachu from "../images/404-error-pokegif.gif";
 
-export default function SeccionPokemon(){
-    const dispatch= useDispatch();
-    const pokemons= useSelector(state=>state.pokemons);
-    const pokemonsDisplay = useSelector(state=>state.pokemonsDisplay);
+export default function SeccionPokemon() {
+    const dispatch = useDispatch();
+    const pokemons = useSelector(state => state.pokemons);
+    const pokemonsDisplay = useSelector(state => state.pokemonsDisplay);
     const [isFilterOpen, setIsFilterOpen] = useState(false);
     const [index, setIndex] = useState(0);
     const [pages, setPages] = useState([]);
 
-    const cardPerPag= 12;
+    const cardPerPag = 12;
 
-    const handlePag = (e,i)=>{
-        if(e.target.textContent==="Prev"){
-            setIndex((prevState)=>prevState-cardPerPag);
-        }else if(e.target.textContent==="Next"){
-            setIndex((prevState)=>prevState+cardPerPag);
-        }else{
-            setIndex(i*cardPerPag);
+    const handlePag = (e, i) => {
+        if (e.target.textContent === "Prev") {
+            setIndex((prevState) => prevState - cardPerPag);
+        } else if (e.target.textContent === "Next") {
+            setIndex((prevState) => prevState + cardPerPag);
+        } else {
+            setIndex(i * cardPerPag);
         }
-        
-        if(e.target.offsetTop>700){
+
+        if (e.target.offsetTop > 700) {
             smoothScroll(e.target.offsetTop);
         }
     }
 
-    useEffect(()=>{
-        if(!pokemons){
+    useEffect(() => {
+        if (!pokemons) {
             dispatch(getAllPokemons());
         } else {
-            dispatch(setPokemons());   
+            dispatch(setPokemons());
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [dispatch]);
 
-    useEffect(()=>{
-        const newPag= Math.ceil(pokemonsDisplay?.length/cardPerPag)
+    useEffect(() => {
+        const newPag = Math.ceil(pokemonsDisplay?.length / cardPerPag)
 
-        if(newPag<pages.length || isNaN(newPag)){
+        if (newPag < pages.length || isNaN(newPag)) {
             setIndex(0);
         }
         setPages(paging(newPag));
@@ -55,42 +55,42 @@ export default function SeccionPokemon(){
 
     return (
         <div className={style.flexContainer}>
-            
+
             {isFilterOpen && pokemonsDisplay &&
-            <div className={style.filter}>
-                <Filter />
-            </div>
+                <div className={style.filter}>
+                    <Filter />
+                </div>
             }
-            
+
             <div className={style.flexcolum}>
 
                 <div className={style.tools}>
-                    {pokemonsDisplay?.length > 1 || isFilterOpen ? 
-                    <button onClick={()=>setIsFilterOpen(!isFilterOpen)}>
-                        {isFilterOpen? "Close Filter" : "Filter / Sort" }
-                    </button>
-                    :<></>}
-                    {pokemonsDisplay && <Search filter={()=>setIsFilterOpen(false)} />}
+                    {pokemonsDisplay?.length > 1 || isFilterOpen ?
+                        <button onClick={() => setIsFilterOpen(!isFilterOpen)}>
+                            {isFilterOpen ? "Close Filter" : "Filter / Sort"}
+                        </button>
+                        : <></>}
+                    {pokemonsDisplay && <Search filter={() => setIsFilterOpen(false)} />}
                 </div>
 
                 <div className={style.section}>
-                    {pokemonsDisplay?.error? 
-                    <div>
-                        <img style={{width:"100%"}} src={errorPikachu} alt="error"/>
-                        <span className={style.span}>{pokemonsDisplay.error}</span>
-                    </div>
-                    :pokemonsDisplay?.length? 
-                    pokemonsDisplay.slice(index,index+cardPerPag).map(pokemon=><CardPokemon key={pokemon.id} id={pokemon.id} image={pokemon.image} Types={pokemon.Types} name={pokemon.name}/>) 
-                    :<div>
-                        <img style={{width:"100%"}} src={loadingPikachu} alt="loading"/>
-                        <span className={style.span}>loading</span>
-                    </div>}
+                    {pokemonsDisplay?.error ?
+                        <div>
+                            <img style={{ width: "100%" }} src={errorPikachu} alt="error" />
+                            <span className={style.span}>{pokemonsDisplay.error}</span>
+                        </div>
+                        : pokemonsDisplay?.length ?
+                            pokemonsDisplay.slice(index, index + cardPerPag).map(pokemon => <CardPokemon key={pokemon.id} id={pokemon.id} image={pokemon.image} Types={pokemon.Types} name={pokemon.name} />)
+                            : <div>
+                                <img style={{ width: "100%" }} src={loadingPikachu} alt="loading" />
+                                <span className={style.span}>loading</span>
+                            </div>}
                 </div>
 
                 <div className={style.pagination}>
-                    {<button value="prev" disabled={index>0?false:true} onClick={(e)=>handlePag(e)}>Prev</button>}
-                    {pages.map((b,i)=><button className={index+cardPerPag===b*cardPerPag?style.active:""} key={i} onClick={(e)=>handlePag(e,i)}>{b}</button>)}
-                    {<button disabled={pokemonsDisplay?.length-index>cardPerPag?false:true} onClick={(e)=>handlePag(e)}>Next</button>}
+                    {<button value="prev" disabled={index > 0 ? false : true} onClick={(e) => handlePag(e)}>Prev</button>}
+                    {pages.map((b, i) => <button className={index + cardPerPag === b * cardPerPag ? style.active : ""} key={i} onClick={(e) => handlePag(e, i)}>{b}</button>)}
+                    {<button disabled={pokemonsDisplay?.length - index > cardPerPag ? false : true} onClick={(e) => handlePag(e)}>Next</button>}
                 </div>
 
             </div>
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,45 +1,48 @@
-export const SET_POKEMONS="SET_POKEMONS";
-export const GET_ALL_POKEMONS="GET_ALL_POKEMONS";
-export const SEARCH_POKEMON="SEARCH_POKEMON";
-export const CLEAR_DISPLAY="CLEAR_DISPLAY";
-export const GET_TYPES="GET_TYPES";
-export const FILTER_AND_ORDER="FILTER_AND_ORDER";
+export const SET_POKEMONS = "SET_POKEMONS";
+export const GET_ALL_POKEMONS = "GET_ALL_POKEMONS";
+export const SEARCH_POKEMON = "SEARCH_POKEMON";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const GET_TYPES = "GET_TYPES";
+export const FILTER_AND_ORDER = "FILTER_AND_ORDER";
 
 export const getAllPokemons = () => dispatch => {
     return fetch(`${process.env.REACT_APP_API}/pokemons`)
-    .then(response=> response.json())
-    .then(data => {
-        return dispatch({type: GET_ALL_POKEMONS, payload: data});
-    })
+        .then(response => response.json())
+        .then(data => {
+            return dispatch({ type: GET_ALL_POKEMONS, payload: data });
+        })
+        .catch((error) => {
+            alert(`El servidor está desplegado 🚀 en Railway.app de manera gratuita, y en estos momentos se encuentra fuera de servicio, puedes mirar el repositorio y probarlo localmente. O puedes intentarlo otro día. Generalmente el servicio se encuentra activo del 1 al 20 de cada mes. Discule las molestias, gracias por su visita! 💛`)
+        })
 };
 
 export const searchPokemon = (name) => dispatch => {
     return fetch(`${process.env.REACT_APP_API}/pokemons?name=${name}`)
-    .then(response=> response.json())
-    .then(data => {
-        return dispatch({type: SEARCH_POKEMON, payload: data});
-    })
-    .catch(error => {
-        return dispatch({type: SEARCH_POKEMON, payload:{error:error}});
-    })
+        .then(response => response.json())
+        .then(data => {
+            return dispatch({ type: SEARCH_POKEMON, payload: data });
+        })
+        .catch(error => {
+            return dispatch({ type: SEARCH_POKEMON, payload: { error: error } });
+        })
 };
 
 export const getTypes = () => dispatch => {
     return fetch(`${process.env.REACT_APP_API}/types`)
-    .then(response=> response.json())
-    .then(data => {
-        return dispatch({type: GET_TYPES, payload: data});
-    })
+        .then(response => response.json())
+        .then(data => {
+            return dispatch({ type: GET_TYPES, payload: data });
+        })
 };
 
-export const clearDisplay = ()=> {
-    return {type: CLEAR_DISPLAY};
+export const clearDisplay = () => {
+    return { type: CLEAR_DISPLAY };
 };
 
-export const setPokemons = ()=> {
-    return {type: SET_POKEMONS};
+export const setPokemons = () => {
+    return { type: SET_POKEMONS };
 };
 
-export const filterAndOrder = (pokemons)=> {
-    return {type: FILTER_AND_ORDER, payload: pokemons};
+export const filterAndOrder = (pokemons) => {
+    return { type: FILTER_AND_ORDER, payload: pokemons };
 };
